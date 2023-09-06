4fcaa60bf1042b7ff5bb4236b5017ce20c22a2f5
kwarnerredhat <kwarner@redhat.com>
2023-09-06 12:59:05 -0400

    color

--- src/components/ColoredTd.tsx
+++ src/components/ColoredTd.tsx
@@ -1,6 +1,6 @@
 import { Td, TdProps } from '@patternfly/react-table';
 import { createUseStyles } from 'react-jss';
-import { choosableColors, Customer, getCustomers } from 'src/api/CustomerApi';
+import { Color as ApiColor } from 'src/api/CustomerApi';
 
 type Color = string;
 
