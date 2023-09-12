0209112329a66df95bc3da0170c9e1b0f0c79597
kwarnerredhat <kwarner@redhat.com>
2023-09-12 16:51:45 -0400

    Just wanted feedback on my progress so far

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -16,22 +16,15 @@ import {
 import { Caption, TableComposable, Tbody, Th, Thead, Tr } from '@patternfly/react-table';
 import { FormEvent, useState } from 'react';
 import { createUseStyles } from 'react-jss';
-import { useQuery, QueryClient, QueryClientProvider, } from 'react-query';
+import { useQuery } from 'react-query';
 import { choosableColors, Customer, getCustomers } from 'src/api/CustomerApi';
 import { ColoredTd } from 'src/components/ColoredTd';
 import Loader from 'src/components/Loader';
 import { useAppContext } from 'src/middleware';
 import ReactDOM from 'react-dom';
-import App from './App';
 
-const queryClient = new QueryClient();
 
-ReactDOM.render(
-  <QueryClientProvider client={queryClient}>
-    <App />
-  </QueryClientProvider>,
-  document.getElementById('root')
-);
+
 const useStyles = createUseStyles({
   inlineText: {
     display: 'block',
@@ -70,9 +63,16 @@ export default () => {
         </Button>
       </GridItem>
       <GridItem sm={6}>
-        <Button onClick={() => setIsModalOpen(true)} variant='secondary'>
+        <Button
+          onClick={() => {
+
+            mutate({});
+          }}
+          variant='secondary'
+        >
           Add New Customer
         </Button>
+
       </GridItem>
       <Modal
         variant={ModalVariant.small}
