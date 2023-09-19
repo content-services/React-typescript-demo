5c3935d365c4a1866f75d8a792d4d13e4124381f
kwarnerredhat <kwarner@redhat.com>
2023-09-19 11:37:15 -0400

    My completed snazzy button

--- src/components/SnazzyButton.tsx
+++ src/components/SnazzyButton.tsx
@@ -1,18 +1,34 @@
 import React from 'react';
 import { createUseStyles } from 'react-jss';
-import snazzyButtonStyling from './snazzyButtonStyling';
+
+
+const styles = {
+  snazzyButton: {
+
+    backgroundColor: 'blue',
+    color: 'white',
+    padding: '10px 20px',
+    borderRadius: '5px',
+
+  },
+  normalButton: {
+
+    backgroundColor: 'gray',
+    color: 'black',
+    padding: '10px 20px',
+    borderRadius: '5px',
+
+  },
+};
+
+const useStyles = createUseStyles(styles);
+
 interface SnazzyButtonProps {
   isSnazzy: boolean;
-
 }
 
-
-const useStyles = createUseStyles(snazzyButtonStyling);
-
 const SnazzyButton: React.FC<SnazzyButtonProps> = ({ isSnazzy, ...rest }) => {
-
   const classes = useStyles();
-
 
   return (
     <button className={isSnazzy ? classes.snazzyButton : classes.normalButton} {...rest}>
