b68011a7e890af685f6abecc916ccd01952e77b6
kwarnerredhat <kwarner@redhat.com>
2023-09-20 14:00:46 -0400

    I changed it up

--- src/components/SnazzyButton.tsx
+++ src/components/SnazzyButton.tsx
@@ -1,49 +1,15 @@
 import React from 'react';
 import { createUseStyles } from 'react-jss';
 import { snazzyButtonStyle } from './helpers';
-import isSnazzy from 'src/components/SnazzyButton';
+
+
 interface ButtonProps {
   children: React.ReactNode;
 }
 
 const styles = {
   snazzyButton: {
-    border: '1px solid currentColor',
-    borderRadius: '5px',
-    color: '#0066CC!important',
-    backgroundColor: 'transparent!important',
-    overflow: 'hidden',
-    position: 'relative',
-    textDecoration: 'none',
-    transition: 'all .2s ease-in-out',
-    willChange: 'transform',
-    zIndex: '0',
-    '&:after': {
-      background: 'linear-gradient(90deg,#19547b, #ffd89b, #19547b)',
-      content: '',
-      display: 'block',
-      height: '100%',
-      width: '100%',
-      position: 'absolute',
-      left: '0',
-      top: '0',
-      transform: 'translate(-100%, 0) rotate(10deg)',
-      transformOrigin: 'top left',
-      transition: '.2s transform ease-out',
-      willChange: 'transform',
-      zIndex: '-1',
-    },
-    '&:hover:after': {
-      transform: 'translate(0, 0)',
-    },
-    '&:hover': {
-      fontWeight: '600',
-      border: '1px solid transparent',
-      color: 'white!important',
-      transform: 'scale(1.05)',
-      willChange: 'transform',
 
-    },
   }
 };
 
@@ -53,11 +19,20 @@ interface SnazzyButtonProps {
   isSnazzy: boolean;
 }
 
-const SnazzyButton: React.FC<SnazzyButtonProps> = ({ children, ...rest }) => {
+const SnazzyButton: React.FC<SnazzyButtonProps> = ({ isSnazzy, children, ...rest }) => {
   const classes = useStyles();
 
+  const classNames = {
+    [classes.snazzyButton]: isSnazzy,
+
+  };
+
+  const className = Object.keys(classNames)
+    .filter((key) => classNames[key])
+    .join(' ');
+
   return (
-    <button className="isSnazzy ? classes.snazzyButton :" {...rest}>
+    <button className={className} {...rest}>
       {children}
     </button>
   );
