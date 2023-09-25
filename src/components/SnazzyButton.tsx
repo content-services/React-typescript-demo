a848208d654bfb1fb494b773f8b01ad259cce248
kwarnerredhat <134307911+kwarnerredhat@users.noreply.github.com>
2023-09-25 11:49:34 -0400

    Task 2: Snazzy Button (#6)
    * Task 2: Snazzy Button
    
    * My changes
    
    * My new snazzy compnent
    
    * My completed snazzy button
    
    * Now my completed snazzy button
    
    * Went based off of comments
    
    * I changed it up
    
    * I should be done
    
    * Completed 2
    
    * used it

---
+++ src/components/SnazzyButton.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { createUseStyles } from 'react-jss';
+import { snazzyButtonStyle } from './helpers';
+import { Button } from '@patternfly/react-core';
+
+const styles = {
+  snazzyButton: snazzyButtonStyle,
+};
+
+const useStyles = createUseStyles(styles);
+
+interface SnazzyButtonProps {
+  isSnazzy: boolean;
+  children: React.ReactNode;
+}
+
+const SnazzyButton: React.FC<SnazzyButtonProps> = ({ isSnazzy, children, ...rest }) => {
+  const classes = useStyles();
+
+  return (
+    <Button className={isSnazzy ? classes.snazzyButton : ''} {...rest}>
+      {children}
+    </Button>
+  );
+};
+
+export default SnazzyButton;
