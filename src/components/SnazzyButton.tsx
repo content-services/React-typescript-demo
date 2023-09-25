2d1fbdcfd749714403603b8347b06307ec7373e2
kwarnerredhat <kwarner@redhat.com>
2023-09-25 11:42:55 -0400

    used it

--- src/components/SnazzyButton.tsx
+++ src/components/SnazzyButton.tsx
@@ -3,12 +3,8 @@ import { createUseStyles } from 'react-jss';
 import { snazzyButtonStyle } from './helpers';
 import { Button } from '@patternfly/react-core';
 
-
-
 const styles = {
-  snazzyButton: snazzyButtonStyle
-
-
+  snazzyButton: snazzyButtonStyle,
 };
 
 const useStyles = createUseStyles(styles);
@@ -21,12 +17,10 @@ interface SnazzyButtonProps {
 const SnazzyButton: React.FC<SnazzyButtonProps> = ({ isSnazzy, children, ...rest }) => {
   const classes = useStyles();
 
-
-
   return (
-    <button className={isSnazzy ? classes.snazzyButton : ' '} {...rest}>
+    <Button className={isSnazzy ? classes.snazzyButton : ''} {...rest}>
       {children}
-    </button>
+    </Button>
   );
 };
 
