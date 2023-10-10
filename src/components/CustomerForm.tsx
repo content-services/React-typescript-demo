c067c11b641f0f6ef17c53bca9a03074d5b63272
kwarnerredhat <kwarner@redhat.com>
2023-10-10 09:53:21 -0400

    What i got so far

---
+++ src/components/CustomerForm.tsx
@@ -0,0 +1,38 @@
+import React, { useState, FormEvent } from 'react';
+import { useQuery, useQueryClient } from 'react-query';
+
+
+interface Customer {
+
+  isCool: boolean;
+}
+
+async function getCustomers() {
+  getCustomers
+}
+
+async function addNewCustomer(customer: Customer) {
+  addNewCustomer
+}
+
+interface CustomerFormProps {
+  setIsModalOpen: (isOpen: boolean) => void;
+}
+
+const CustomerForm: React.FC<CustomerFormProps> = ({ setIsModalOpen }) => {
+  const [newUser, setNewUser] = useState<Partial<Customer>>({ isCool: false });
+  const queryClient = useQueryClient();
+
+  // Queries
+  const { isLoading, data } = useQuery('customers', getCustomers);
+
+  const onSubmit = (e: FormEvent<Element>) => {
+    e.preventDefault();
+    addNewCustomer(newUser as Customer);
+    setNewUser({ isCool: false });
+    setIsModalOpen(false);
+    queryClient.invalidateQueries('customers');
+  };
+
+}
+export default CustomerForm;
